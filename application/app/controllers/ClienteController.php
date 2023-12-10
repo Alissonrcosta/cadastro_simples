0ba9ef4141f1653ddad67cb957ef87ba556549a7
root <root@DESKTOP-34SIK3U>
2023-12-10 12:57:05 -0300

    metodo cadastrar e atualizar implementado

--- application/app/controllers/ClienteController.php
+++ application/app/controllers/ClienteController.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Date as DateValidator;
+
 class ClienteController extends ControllerBase
 {
 
@@ -9,11 +12,66 @@ class ClienteController extends ControllerBase
     }
 
     public function CadastrarAction() {
-        
+    $dados = $this->request->getPost();
+    $email = $dados['email'];
+
+    //verificando se usuário já está cadastrado
+    $consulta = Cliente::find("email = '$email'");
+    if (count($consulta) > 0) {
+        echo "Usuário já cadastrado";
+    }else{
+        $dataNascimento = DateTime::createFromFormat('d/m/Y', $dados['data_nascimento']);
+            if (!$dataNascimento || $dataNascimento->format('d/m/Y') !== $dados['data_nascimento']) {
+                echo 'Data de nascimento inválida.';
+            }
+
+            //validação se data não está no futuro
+    $dataAtual = new DateTime();
+    
+    if ($dataNascimento > $dataAtual) {
+        echo 'Data de nascimento inválida(futuro).';
+    }else{
+    //Continuar para salvar no bando de dados
+    $cliente = new Cliente();
+    $cliente->nome = $dados['nome'];
+    $cliente->email = $dados['email'];
+    $cliente->data_nascimento = $dataNascimento->format('Y-m-d');
+
+    $cliente->save();
+    $this->response->redirect('/');
     }
+    }
+    
+}
+  
+        //echo "<pre>"; print_r($dataNascimento); echo "</pre>"; exit;
+    
     
     public function EditarAction() {
 
     }
 
+    public function AtualizarAction() {
+
+    }
+
+    public function DeletarAction($id) {
+        if(!isset($id)) {
+            echo "Nenhum usuário selecionado";
+        } else {
+            $cliente = Cliente::find("id = '$id'");
+            if ($cliente) {
+                if ($cliente->delete() === false) {
+                    echo "Erro ao deletar o cliente.";
+                } else {
+                    $this->response->redirect('/');
+                }
+            } else {
+                echo "Cliente não encontrado.";
+            }
+        }
+    }
 }
+    
+
+
